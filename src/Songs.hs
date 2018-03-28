1063629d89baa25f9c63c157e698f4c41fa59556
Kent Kawanaka <k-kawanaka@plott.co.jp>
2018-03-28 11:57:52 +0900

    曲選択系の関数を追加

--- src/Songs.hs
+++ src/Songs.hs
@@ -3,13 +3,26 @@
 module Songs where
 
 import GHC.Generics
-import Data.ByteString as B
+import Data.ByteString as B (ByteString)
 import Data.Aeson
+import System.Random
 
 data Song = Song {name :: String, rate :: Int } deriving (Show, Eq, Generic)
+type SongBase = [String]
 
 instance FromJSON Song
 
 createSongs :: B.ByteString -> Either String [Song]
 createSongs = eitherDecodeStrict
+
+createSongBase :: [Song] -> SongBase
+createSongBase = foldr inner [] where
+  inner song acc = acc ++ replicate (rate song) (name song)
+
+
+chooseSong :: SongBase -> StdGen -> String
+chooseSong songBase = (songBase !!) . fst .  randomR (0, length songBase)
+
+chooseSongIO :: SongBase -> IO String
+chooseSongIO songBase = chooseSong songBase <$> newStdGen
 
