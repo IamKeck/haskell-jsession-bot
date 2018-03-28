8075927a18a930869b07162157770a9a26897c04
Kent Kawanaka <k-kawanaka@plott.co.jp>
2018-03-28 15:44:47 +0900

    reply関数を追加

--- src/Operation.hs
+++ src/Operation.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Operation where
 
 import OAuth
 import Network.HTTP.Simple
+import qualified Data.HashMap.Strict as HM
+import Data.Aeson (decode)
+import Data.Aeson.Types
+import qualified Data.Text as T
 
 data CreateFriendship = CreateFriendship {screenName :: Maybe String, userId :: Maybe String, follow :: Maybe Bool}
   deriving Show
@@ -26,6 +31,22 @@ followUser key user = do
   else
     return True
 
-
-
+reply :: TwitterKey -> Object -> String -> IO Bool
+reply key obj message = do
+  case create_reply of
+    Nothing -> print obj >> return False
+    Just reply -> do
+      res <- reply >>= httpBS
+      if getResponseStatusCode res > 300 then
+        (print $ getResponseBody res) >> return False
+      else
+        return True
+  where
+    create_reply = do
+      let url = "https://api.twitter.com/1.1/statuses/update.json"
+      status_id <- HM.lookup "id_str" obj >>= takeString
+      screen_name <-  HM.lookup "user" obj >>= takeObj >>= HM.lookup "screen_name" >>= takeString
+      let reply_message = "@" ++ screen_name ++ " " ++ message
+      let param = [("status", reply_message), ("in_reply_to_status_id", status_id)]
+      return $ createRequest True url [] param key
 
